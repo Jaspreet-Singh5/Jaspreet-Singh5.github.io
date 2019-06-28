7fd4b41a0c25872a3a78d064b7e38ffc352e1e3a
Jaspreet-Singh5 <jaswindersingh705@gmail.com>
2019-06-29 01:50:53 +0530

    Add new script file

--- js/script.js
+++ js/script.js
@@ -4,20 +4,27 @@ function mobList() {
     var nav = document.querySelector(".navigation");
     var navNav = document.querySelector(".navigation__nav");
     var navIcon = document.querySelector(".navigation__icon");
+    var navIconI = document.querySelector(".navigation__icon .fa");
 
     if (x.style.display === "flex") {
+        navIconI.classList.remove("fa-times");
+        navIconI.classList.add("fa-bars");
         navIcon.style.marginLeft = "0rem";
 
-        if (screen.width < 600)
-            navIcon.style.transform = "translateY(-3rem)";
         nav.style.width = "8%";
         x.style.display = "none";
         
 
         if (screen.width < 900) {
             nav.style.backgroundColor = "transparent";
-        }        
+        }
+
+        if (screen.width < 600) {
+            navIcon.style.transform = "translateY(-3rem)";
+        }
     } else {
+        navIconI.classList.remove("fa-bars");
+        navIconI.classList.add("fa-times");
         navIcon.style.marginLeft = "5rem";
 
         if (screen.width > 900)
@@ -28,6 +35,11 @@ function mobList() {
             nav.style.backgroundColor = "#4b7bec";
             nav.style.borderRadius = "0px";
             nav.style.margin = "0px";
+
+            if (screen.width < 600) {
+                nav.style.width = "35%";
+                nav.style.height = "100%";
+            }
         }
        
         x.style.display = "flex";
